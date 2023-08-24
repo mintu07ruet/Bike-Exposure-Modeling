585fe00441a5420356b1c66c72eb4cdacbffc0f5
Frank Proulx <frank@frankproulxconsulting.com>
2023-08-24 16:47:04 +0000

    estimate models more

--- count_modeling/count_modeling/constants.py
+++ count_modeling/count_modeling/constants.py
@@ -9,4 +9,4 @@ MODEL_DATA_PATH = f'{GCS_PATH}/model_data/{{version}}/{{part}}/{{table}}.parquet
 # For storing the results of model runs (model object, accuracy measures, etc.)
 MODEL_PATH = f'{GCS_PATH}/models/{{version}}.pkl'
 
-CURRENT_MODEL_DATA_VERSION = '2023-06-25T07:16:18.063804-07:00'
+CURRENT_MODEL_DATA_VERSION = '2023-07-15T08:13:02.606521-07:00'
